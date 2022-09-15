f92e5e83c4a8b8883ec626f81286e2bc189badcd
Jackson Horton <jacksonhorton12@icloud.com>
2022-09-15 11:25:22 -0500

    Added constructor. Revissed constructor in header file to pass filename and number of files.

--- stacker.cpp
+++ stacker.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include "stacker.h"
+
+using namespace std;
+
+
+Stacker::Stacker(string filename, int num_of_files) {
+  this->filename;
+  this->num_of_files = num_of_files;
+}
+
+
+
